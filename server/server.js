88f504165dad46f933bd7df8918a343fdb97494e
LazyDotExe <papaporo@proton.me>
2023-07-21 20:01:40 -0500

    created files and server

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('server/public'))
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+app.listen(port, () => {
+    console.log('listening on port', port)
+})
